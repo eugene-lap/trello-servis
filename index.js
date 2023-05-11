168a8b1c204dc4cf03dc323bf06e37051788a718
Eugene.Lapa <iapa2002@mail.ru>
2023-05-11 16:09:22 +0300

    cors fix

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    origin: ["*"]
-}))
+app.use(cors())
 app.use("/auth", authRouter)
 
 const start = async () => {
